3989a64dd7a9ff8cd032673753988f7837487374
Junfeng Hu <junfeng.hu@screeningeagle.com>
2022-08-16 17:32:47 +0800

    Fix AttributeContainer conflicting iOS 15's new AttributeContainer

--- SZMentionsSwiftTests/AttributeContainerTests.swift
+++ SZMentionsSwiftTests/AttributeContainerTests.swift
@@ -4,8 +4,8 @@ import XCTest
 private final class AttributeContainerTests: XCTestCase {
     func test_shouldBeGeneratedFromAnArrayOfAttributeContainers() {
         let actual = [
-            Attribute(name: .backgroundColor, value: UIColor.red) as AttributeContainer,
-            Attribute(name: .foregroundColor, value: UIColor.blue) as AttributeContainer,
+            Attribute(name: .backgroundColor, value: UIColor.red) as SZMentionsSwift.AttributeContainer,
+            Attribute(name: .foregroundColor, value: UIColor.blue) as SZMentionsSwift.AttributeContainer,
         ].dictionary as! [NSAttributedString.Key: UIColor]
         let expected: [NSAttributedString.Key: UIColor] = [NSAttributedString.Key.backgroundColor: UIColor.red,
                                                            NSAttributedString.Key.foregroundColor: UIColor.blue]
